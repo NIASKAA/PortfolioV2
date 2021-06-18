01911ae2dfb60002f2c2b2be3638a67d0ce1e9eb
NIASKA <mkman751@yahoo.com>
2021-06-18 14:35:57 -0500

    updated project page

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,7 @@
 const tl = gsap.timeline({default: {ease: "power1.out"} });
 
-tl.to(".cText", {y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", {y: "0%", duration: 1.5, delay: 0.5 });
-tl.to(".coverPage", {y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo(".titleText", {opacity: 0}, {opacity: 1, duration: 0.5});
+tl.fromTo("navbar", {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo(".mainText", {opacity: 0}, {opacity: 1, duration: 0.5});
 
 const navButton = () => {
     const naviBtn = document.querySelector('.resButton');
