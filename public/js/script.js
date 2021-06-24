a8d69cf3ccc058ac392512ccef5006cc148a9dd7
NIASKA <mkman751@yahoo.com>
2021-06-24 15:04:55 -0500

    added navbar animations

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,5 @@
 const tl = gsap.timeline({default: {ease: "power1.out"} });
+tl.fromTo(".navbar", {opacity: 0}, {opacity: 1, duration: 0.5, stagger: 0.3});
+tl.fromTo("#mainText", {opacity: 0}, {opacity: 1, duration: 0.8});
+tl.fromTo("#secondText", {opacity: 0}, {opacity: 1, duration: 0.15});
 
-tl.fromTo("navbar", {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo(".mainText", {opacity: 0}, {opacity: 1, duration: 0.5});
-
-const navButton = () => {
-    const naviBtn = document.querySelector('.resButton');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    naviBtn.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = ''
-            }else {
-                link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.6}s `;
-            }
-        });
-    });
-
-    
-}
-
-navButton();
